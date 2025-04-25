18fc30542ee01f8bcb1bbb80da755ee881d665c2
rohanmaharana1723 <135946243+rohanmaharana1723@users.noreply.github.com>
2025-04-25 08:51:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 
 # Load model, encoder, and scaler
-model = pickle.load(open("car_price_model.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
 encoder = pickle.load(open("encoder.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
